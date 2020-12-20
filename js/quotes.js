91a8ade478ce42e4ea0ebb3189413520ccb2370b
guithead <62117784+guithead@users.noreply.github.com>
2020-12-20 15:14:40 +0100

    Add files via upload

---
+++ js/quotes.js
@@ -0,0 +1,50 @@
+const bukowskiQuotes = [
+  {
+    text: "Find what you love and let it kill you.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "Sometimes you just have to pee in the sink.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "Some people never go crazy. What truly horrible lives they must live.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "I’m just an alcoholic who became a writer so that I would be able to stay in bed until noon. ",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "She was desperate and she was choosey at the same time and, in a way, beautiful, but she didn’t have quite enough going for her to become what she imagined herself to be.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "We see it too late: after the cock gets swallowed the heart follows.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "And you invented me and I invented you and that’s why we don’t get along.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "And yet women –– good women –– frightened me because they eventually wanted your soul, and what was left of mine, I wanted to keep.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "Still, I kept thinking about Lydia. The good parts of our relationship felt like a rat walking around and gnawing at the inside of my stomach.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "He asked, What makes a man a writer? Well, I said, it's simple. You either get it down on paper, or jump off a bridge.",
+    author: "Charles Bukowski",
+  },
+];
---
+++ js/script.js
@@ -0,0 +1,52 @@
+//Variables
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Remove Loading Spinner
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+//Show new Quote
+function newQuote() {
+  loading();
+  //Get a random quote locally from bukowski.js
+  const quote =
+    bukowskiQuotes[Math.floor(Math.random() * bukowskiQuotes.length)];
+  console.log(quote);
+
+  authorText.textContent = quote.author;
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+//On Load
+newQuote();
