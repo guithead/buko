930caf6083d681d3edd36a36edd86f2a43820b96
guithead <62117784+guithead@users.noreply.github.com>
2020-12-21 19:00:10 +0100

    Add files via upload

--- js/quotes.js
+++ js/quotes.js
@@ -19,11 +19,6 @@ const bukowskiQuotes = [
   },
   {
     text:
-      "She was desperate and she was choosey at the same time and, in a way, beautiful, but she didn’t have quite enough going for her to become what she imagined herself to be.",
-    author: "Charles Bukowski",
-  },
-  {
-    text:
       "We see it too late: after the cock gets swallowed the heart follows.",
     author: "Charles Bukowski",
   },
@@ -34,17 +29,85 @@ const bukowskiQuotes = [
   },
   {
     text:
-      "And yet women –– good women –– frightened me because they eventually wanted your soul, and what was left of mine, I wanted to keep.",
-    author: "Charles Bukowski",
-  },
-  {
-    text:
-      "Still, I kept thinking about Lydia. The good parts of our relationship felt like a rat walking around and gnawing at the inside of my stomach.",
-    author: "Charles Bukowski",
-  },
-  {
-    text:
       "He asked, What makes a man a writer? Well, I said, it's simple. You either get it down on paper, or jump off a bridge.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "Great art is horse shit, buy tacos.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "People run from rain but sit in bathtubs full of water.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "I guess the only time most people think about injustice is when it happens to them.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "Poetry is what happens when nothing else can.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "Dogs and angels are not very far apart.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "Genius might be the ability to say a profound thing in a simple way.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "Beware of those who seek constant crowds, they are nothing alone.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "We drink our coffee and pretend not to look at each other.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "People were usually much better in their letters than in reality. They were much like poets in this way. ",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "Sandra is the slim tall ear-ringed bedroom damsel dressed in a long gown, she’s always high in heels, spirit, pills and booze.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "She knew what she wanted and it wasn’t me.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "I know this poet: he’s just like the rest of us: he’ll vomit anywhere for money.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "And if you have the ability to love, love yourself first.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "There is always one woman to save you from another.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "Cheer up. Maybe you’ll be famous after you’re dead.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "She must have a suction pump for a pussy.",
+    author: "Charles Bukowski",
+  },
+  {
+    text:
+      "Look at his hands. He has the most beautiful hands. You can see that he has never worked.",
+    author: "Charles Bukowski",
+  },
+  {
+    text: "We really had nothing to do but drink wine and make love.",
     author: "Charles Bukowski",
   },
 ];
